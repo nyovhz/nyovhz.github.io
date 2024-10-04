a99e405ff8a7e5645c511f3ec854c9f147ff9d94
nyovhz <nyovhz.dev@gmail.com>
2024-10-04 16:57:08 -0600

    base

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Head from 'next/head';
 
 const trenchFont = localFont({
   src: "./fonts/TrenchThin.ttf",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+'use client'
+import ThreeScene from './components/ThreeScene';
 export default function Home() {
   return (
     <div>
       <h1 role="home-title" className="text-center text-green-500">nyovhz</h1>
+      <ThreeScene />
     </div>
   );
 }
