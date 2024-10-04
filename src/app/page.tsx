c17fa9acef3422765424bc5d7f10a22aedee09d5
nyovhz <nyovhz.dev@gmail.com>
2024-10-04 15:53:23 -0600

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 role="home-title">nyovhz</h1>
+      <h1 role="home-title" className="text-center text-green-500">nyovhz</h1>
     </div>
   );
 }
