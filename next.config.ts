2a24536f9cb9bf3a48051c0bff8747c17136bbcd
nyovhz <nyovhz.dev@gmail.com>
2026-01-31 20:14:41 -0600

    Fix basePath and assetPrefix for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
 
+  basePath: "/nyovhz.github.io",
+  assetPrefix: "/nyovhz.github.io/",
+
   images: {
     unoptimized: true,
     remotePatterns: [
